3d3dc774aad9093d9a41c1f5d0e8e4f5d43c0f89
Ran Reichman <ranreichman@rans-macbook-pro.local>
2017-11-21 16:56:08 -0800

    Removed some commented out code

--- iOSapp/MenuViewController.swift
+++ iOSapp/MenuViewController.swift
@@ -11,33 +11,17 @@ import UIKit
 class MenuViewController : UIViewController
 {
     @IBOutlet weak var termsOfServiceButton: UIButton!
-    //@IBOutlet weak var termsOfServiceButton: UIButton!
-    //@IBOutlet weak var privacyPolicyButton: UIButton!
-    //@IBOutlet weak var termsOfServiceButton: UIButton!
     @IBOutlet weak var privacyPolicyButton: UIButton!
     
     @IBAction func clickedTermsOfServiceButton(_ sender: UIButton)
     {
-        print("clicked terms of service")
         UIApplication.shared.open(URL(string: SystemVariables().TERMS_OF_SERVICE_URL)!, options: [:], completionHandler: nil)
     }
     
     @IBAction func clickedPrivacyPolicyButton(_ sender: UIButton)
     {
-        print("clicked privacy policy")
         UIApplication.shared.open(URL(string: SystemVariables().PRIVACY_POLICY_URL)!, options: [:], completionHandler: nil)
     }
-    /*
-    
-    @IBAction func clickedPrivacyPolicy(_ sender: Any)
-    {
-        print("clicked privacy policy")
-    }*/
-    
-    /*@IBAction func clickTermsOfService(_ sender: UIButton)
-    {
-        print("clicked terms")
-    }*/
     
     override func viewDidLoad()
     {
